eb0fafc203167fadb2b3b43e33da7457f5d5ca6b
intejarhasnathprodhanalif <intejaralif@gmail.com>
2026-08-19 00:32:53 +0600

    1

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Lab1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+        System.out.println("This is Alif");
+    }
 }
